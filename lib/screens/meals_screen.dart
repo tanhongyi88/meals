23cf112dd9afbd9faa7b825055b8cce351bae0e5
tanhongyi88 <hongyi.tan88@gmail.com>
2024-08-28 20:08:24 +0800

    now category has its meals with images

--- lib/screens/meals_screen.dart
+++ lib/screens/meals_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({
@@ -14,12 +15,8 @@ class MealsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Widget content = ListView.builder(
-      itemCount: meals.length,
-      itemBuilder: ((ctx, index) => Text(
-            meals[index].title,
-            style: const TextStyle(color: Colors.white),
-          )),
-    );
+        itemCount: meals.length,
+        itemBuilder: (ctx, index) => MealItem(meal: meals[index]));
     if (meals.isEmpty) {
       content = Center(
         child: Column(
