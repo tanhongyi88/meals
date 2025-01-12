fab9e2e0a32a81ac088c0a67b4e24ee73a427433
tanhongyi88 <hongyi.tan88@gmail.com>
2025-01-12 16:40:45 +0800

    added filter to meals

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
 class MainDrawer extends StatelessWidget {
-  const MainDrawer({super.key});
+  const MainDrawer({
+    super.key,
+    required this.onSelectScreen,
+  });
+
+  final void Function(String identifier) onSelectScreen;
 
   @override
   Widget build(BuildContext context) {
@@ -47,7 +52,9 @@ class MainDrawer extends StatelessWidget {
                     fontSize: 24,
                   ),
             ),
-            onTap: () {},
+            onTap: () {
+              onSelectScreen("meals");
+            },
           ),
           ListTile(
             leading: Icon(
@@ -62,7 +69,9 @@ class MainDrawer extends StatelessWidget {
                     fontSize: 24,
                   ),
             ),
-            onTap: () {},
+            onTap: () {
+              onSelectScreen("filters");
+            },
           ),
         ],
       ),
