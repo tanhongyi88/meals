b74045a326d1331638c5c2d025caecb139238dda
tanhongyi88 <hongyi.tan88@gmail.com>
2025-01-23 22:38:10 +0800

    outsource filtered meals to another provider that depends on meals and filters providers

--- lib/providers/favourites_provider.dart
+++ lib/providers/favourites_provider.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/providers/filters_provider.dart';
+import 'package:meals/providers/meals_provider.dart';
 
 class FavouriteMealsNotifier extends StateNotifier<List<Meal>> {
   FavouriteMealsNotifier() : super([]);
@@ -20,3 +22,24 @@ class FavouriteMealsNotifier extends StateNotifier<List<Meal>> {
 final favouriteMealsProvider =
     StateNotifierProvider<FavouriteMealsNotifier, List<Meal>>(
         (ref) => FavouriteMealsNotifier());
+
+final filteredMealsProvider = Provider((ref) {
+  final meals = ref.watch(mealsProvider);
+  final activeFilters = ref.watch(filtersProvider);
+
+  return meals.where((meal) {
+    if (activeFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
+      return false;
+    }
+    if (activeFilters[Filter.lactoseFree]! && !meal.isLactoseFree) {
+      return false;
+    }
+    if (activeFilters[Filter.vegetarian]! && !meal.isVegetarian) {
+      return false;
+    }
+    if (activeFilters[Filter.vegan]! && !meal.isVegan) {
+      return false;
+    }
+    return true;
+  }).toList();
+});
