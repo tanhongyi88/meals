84b5f1123ac9440d8f281d9bc8db3206722c813a
tanhongyi88 <hongyi.tan88@gmail.com>
2025-02-15 13:51:55 +0800

    implicit animation with animated switcher and rotation transition

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -31,7 +31,19 @@ class MealDetailsScreen extends ConsumerWidget {
                       ? 'Meal added as a favourite'
                       : 'Meal removed')));
             },
-            icon: Icon(isFavourite ? Icons.star : Icons.star_border),
+            icon: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 300),
+              transitionBuilder: (child, animation) {
+                return RotationTransition(
+                  turns: Tween<double>(begin: 0.7, end: 1).animate(animation),
+                  child: child,
+                );
+              },
+              child: Icon(
+                isFavourite ? Icons.star : Icons.star_border,
+                key: ValueKey(isFavourite),
+              ),
+            ),
           ),
         ],
       ),
